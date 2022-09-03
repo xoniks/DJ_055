d6054e77a7e5fc0a44dbd7358b1d50b146f2ebfb
xonidjango <110489575+xonidjango@users.noreply.github.com>
2022-09-03 12:29:30 +0200

    before_test

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from re import template
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
